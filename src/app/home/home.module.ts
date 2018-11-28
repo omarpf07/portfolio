6b809eaf80b6253577b0d04ebd3c32ca9115e00d
Omar Perozo <operozo@intelenz.com>
2018-11-28 14:29:58 -0400

    tes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
-
 import { HomeComponent } from './home.component';
 
 @NgModule({
