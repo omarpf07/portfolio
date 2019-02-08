98ab625c00e0b7a58e2b449bd7b7bc8559125be1
Unknown <omar.perozo07@gmail.com>
2019-02-07 23:06:49 -0400

    advances in testimonials idea

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,17 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 
 import {NgsRevealModule} from 'ngx-scrollreveal';
+import { NguCarouselModule } from '@ngu/carousel';
+
+
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
     SharedModule,
-    NgsRevealModule
+    NgsRevealModule,
+    NguCarouselModule
   ]
 })
 export class HomeModule { }
